8bad5adec9fbde8bfea0fe4ec19d02e4d3a697cd
X00091622 <x00091622@COMPUTING.STU.IT-TALLAGHT.IE>
2019-03-25 11:23:56 +0000

    made my first change

--- project/js/main.js
+++ project/js/main.js
@@ -6,6 +6,16 @@ function myFunction() {
   $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+  $("#doge-image").append(`<img class="img-circle" src="images/guy.jpg" />`);
+}
+
+function my2NdFunction() {
+  // use jQuery ($ is shorthand) to find the div on the page and then change the html
+  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+
+  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+
+  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
